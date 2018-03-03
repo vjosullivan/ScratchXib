a567fa187496ede185713d3393d1b225c6e07169
vjosullivan <vjosullivan@hotmail.com>
2018-03-03 08:24:09 +0000

    Create empty MVVM skeleton

---
+++ ScratchXib/Person/PersonViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PersonViewModel.swift
+//  ScratchXib
+//
+//  Created by Vincent O'Sullivan on 03/03/2018.
+//  Copyright © 2018 Vincent O'Sullivan. All rights reserved.
+//
+
+import Foundation
+
+class PersonViewModel {
+    
+}
